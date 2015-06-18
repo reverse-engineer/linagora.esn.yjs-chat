5d434f980af0101ab7cb0afc754ca129267b0879
Stephen Le Maistre <slemaistre@linagora.com>
2015-06-18 18:04:36 +0200

    MEET-389 now close the bubble popover after 4 seconds

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,6 +3,6 @@
 angular.module('esn.chat')
   .constant('CHAT_AVATAR_SIZE', 48)
   .constant('CHAT_WINDOW_SIZE', { width: 30, height: 50 })
-  .constant('CHAT_POPOVER_DELAY', { show: 1000, hide: 200 })
-  .constant('DEFAULT_AVATAR', '/images/avatar/default.png');
-
+  .constant('CHAT_POPOVER_DELAY', { show: 100, hide: 200 })
+  .constant('DEFAULT_AVATAR', '/images/avatar/default.png')
+  .constant('CHAT_HIDE_TIMEOUT', 4000);
