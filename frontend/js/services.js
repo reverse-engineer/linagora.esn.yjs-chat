5d12c4d206b52790ccfba468ffa93962bd5aa3aa
Corentin STG_CADIOU <ccadiou@linagora.com>
2015-06-18 12:27:20 +0200

    MEET-396 chat angular factory

--- frontend/js/services.js
+++ frontend/js/services.js
@@ -1,59 +1,97 @@
 'use strict';
 
 angular.module('esn.chat')
-.factory('ChatMessage', function() {
+  .factory('ChatMessage', function() {
 
-  function ChatMessage(object) {
-    this.author = object.author;
-    this.authorAvatar = object.authorAvatar;
-    this.published = object.published;
-    this.message = object.message;
-  }
+    function ChatMessage(object) {
+      this.author = object.author;
+      this.authorAvatar = object.authorAvatar;
+      this.published = object.published;
+      this.message = object.message;
+    }
 
-  return ChatMessage;
-})
-.factory('yArraySynchronizer', ['yjsService', '$window', function(yjsService, $window) {
+    return ChatMessage;
+  })
+  .factory('yArraySynchronizer', ['yjsService', '$window', function(yjsService, $window) {
 
-  function onChange(jsArray) {
-    return function(events) {
-      events.forEach(function(event) {
-        var type = event.type,
-            value = event.value,
-            position = event.position;
-        if (type === 'delete') {
-          jsArray.splice(position, 1);
-        } else if (type === 'insert') {
-          jsArray.splice(position, 0, value);
-        } else if (type === 'update') {
-          jsArray[position] = value;
+    function onChange(jsArray) {
+      return function(events) {
+        events.forEach(function(event) {
+          var type = event.type,
+              value = event.value,
+              position = event.position;
+          if (type === 'delete') {
+            jsArray.splice(position, 1);
+          } else if (type === 'insert') {
+            jsArray.splice(position, 0, value);
+          } else if (type === 'update') {
+            jsArray[position] = value;
+          }
+        });
+      };
+    }
+
+    function mapToYList(id, jsArray, callback) {
+      callback = callback || function() {};
+
+      var y = yjsService().y;
+
+      yjsService().connector.whenSynced(function() {
+        var ylist = y.val(id);
+        if (!ylist) {
+          ylist = new $window.Y.List(jsArray);
+          y.val(id, ylist);
         }
+        ylist.observe(onChange(jsArray));
+        callback(ylist);
       });
-    };
-  }
-
-  function mapToYList(id, jsArray, callback) {
-    if (!id) {
-      throw new Error('id is required');
     }
-    if (!jsArray) {
-      throw new Error('jsArray is required');
-    }
-    if (!angular.isArray(jsArray)) {
-      throw new Error('jsArray should be a javascript array');
-    }
-    callback = callback || function() {};
+    return mapToYList;
+  }])
+  .factory('chat', ['$rootScope', 'yjsService', 'yArraySynchronizer',
+    function($rootScope, yjsService, yArraySynchronizer) {
 
-    var y = yjsService().y;
-
-    yjsService().connector.whenSynced(function() {
-      var ylist = y.val(id);
-      if (!ylist) {
-        ylist = new $window.Y.List(jsArray);
-        y.val(id, ylist);
+      function sendMessage(chatMessage) {
+        if (!chatMessage) {
+          throw new Error('No message provided');
+        }
+        ret.yMessages.push(chatMessage);
+        $rootScope.$emit('chat:message:sent', chatMessage);
       }
-      ylist.observe(onChange(jsArray));
-      callback(ylist);
-    });
-  }
-  return mapToYList;
-}]);
+
+      function toggleWindow() {
+        ret.opened = !ret.opened;
+        ret.unread = ret.opened ? 0 : ret.unread;
+        $rootScope.$emit('chat:window:visibility', {visible: ret.opened});
+      }
+
+      var ret = {
+        yMessages: null,
+        messages: [],
+        opened: false,
+        unread: 0,
+        toggleWindow: toggleWindow,
+        sendMessage: sendMessage
+      };
+
+      var callback = function(yList) {
+        ret.yMessages = yList;
+
+        ret.yMessages.observe(function(events) {
+          events.forEach(function(event) {
+            if (event.type === 'insert') {
+              $rootScope.$emit('chat:message:received', event.value);
+              ret.unread = ret.opened ? 0 : ret.unread + 1;
+            } else if (event.type === 'update') {
+              // Don't do anything for now!
+            } else if (event.type === 'delete') {
+              // Don't do anything for now!
+            }
+          });
+        });
+      };
+
+      yArraySynchronizer('chat:messages', ret.messages, callback);
+
+      return ret;
+    }]);
--- test/unit-frontend/services.js
+++ test/unit-frontend/services.js
@@ -42,30 +42,6 @@ describe('the yArraySynchronizer factory', function() {
 
   });
 
-  it('should throw an error if first argument is undefined', function() {
-    var self = this;
-    function test() {
-      return self.yArraySynchronizer(null);
-    }
-    expect(test).to.throw(/id is required/);
-  });
-
-  it('should throw an error if second argument is undefined', function() {
-    var self = this;
-    function test() {
-      return self.yArraySynchronizer('test');
-    }
-    expect(test).to.throw(/jsArray is required/);
-  });
-
-  it('should throw an error if second argument is not an array', function() {
-    var self = this;
-    function test() {
-      return self.yArraySynchronizer('test', 'not an array');
-    }
-    expect(test).to.throw(/jsArray should be a javascript array/);
-  });
-
   it('should call the callback when yjs isSynced is called', function() {
     this.yServiceData.y = {
       val: function() {
@@ -113,6 +89,231 @@ describe('the yArraySynchronizer factory', function() {
 
     this.yArraySynchronizer('test', myTab, function() {
       done();
+    });
+  });
+
+});
+
+describe('the chat factory', function() {
+  var chatFactory, yList, $rootScope, message;
+
+  beforeEach(module('esn.chat'));
+  beforeEach(function() {
+    var self = this;
+    var yService = function() {
+      return {
+        connector: {
+          whenSynced: function(callback) {
+            callback();
+          }
+        }
+      };
+    };
+
+    yList = {
+      val: chai.spy(),
+      observe: chai.spy(),
+      push: chai.spy()
+    };
+
+    this.yArraySynchronizerMock = function(channel, messages, callback) {
+      callback(yList);
+    };
+
+    var yArraySynchronizer = function(channel, messages, callback) {
+      return self.yArraySynchronizerMock(channel, messages, callback);
+    };
+
+    message = { foo: 'bar' };
+
+    module(function($provide) {
+      $provide.value('yArraySynchronizer', yArraySynchronizer);
+      $provide.value('yjsService', yService);
+    });
+
+    inject(function(_$rootScope_) {
+      $rootScope = _$rootScope_;
+    });
+
+  });
+
+  it('should have the right properties', function() {
+    inject(function(chat) {
+      chatFactory = chat;
+    });
+    expect(chatFactory).to.have.property('yMessages');
+    expect(chatFactory).to.have.property('messages');
+    expect(chatFactory).to.have.property('opened')
+      .and.to.equal(false);
+    expect(chatFactory).to.have.property('unread')
+      .and.to.equal(0);
+  });
+
+  describe('the sendMessage method', function() {
+    beforeEach(function() {
+      inject(function(chat) {
+        chatFactory = chat;
+      });
+    });
+
+    it('should fail if no message is provided', function() {
+
+      var test = function() {
+        chatFactory.sendMessage();
+      };
+      expect(test).to.throw(/No message provided/);
+    });
+
+    it('should push the message in the yjs list', function() {
+      chatFactory.sendMessage(message);
+      expect(yList.push).to.have.been.called.with(message);
+    });
+
+    it('should broadcast a chat:message:sent', function(done) {
+      $rootScope.$on('chat:message:sent', function() { done(); });
+      chatFactory.sendMessage(message);
+      $rootScope.$digest();
+    });
+  });
+
+  describe('the toggleWindow method', function() {
+    var self = this;
+    beforeEach(function() {
+      inject(function(chat) {
+        chatFactory = chat;
+      });
+    });
+
+    it('should toggle the opened boolean', function() {
+      expect(chatFactory.opened).to.be.false;
+      chatFactory.toggleWindow();
+      expect(chatFactory.opened).to.be.true;
+      chatFactory.toggleWindow();
+      expect(chatFactory.opened).to.be.false;
+    });
+
+    describe('unread messages count', function() {
+      it('should be reset when opened becomes true', function() {
+        chatFactory.unread = 42;
+        chatFactory.toggleWindow();
+        expect(chatFactory.unread).to.equal(0);
+      });
+    });
+
+    it('should emit chat:window:visibility event on window opening', function(done) {
+      $rootScope.$on('chat:window:visibility', function(evt, data) {
+        expect(data).to.deep.equal({visible: true});
+        done();
+      });
+      chatFactory.toggleWindow();
+    });
+    it('should emit chat:window:visibility event on window closing', function(done) {
+      chatFactory.toggleWindow();
+      $rootScope.$on('chat:window:visibility', function(evt, data) {
+        expect(data).to.deep.equal({visible: false});
+        done();
+      });
+      chatFactory.toggleWindow();
+    });
+  });
+
+  describe('initialization code', function() {
+    it('should register a yArraySynchronizer array', function(done) {
+      this.yArraySynchronizerMock = function(channel, messages, callback) {
+        expect(channel).to.equal('chat:messages');
+        expect(messages).to.be.an('array');
+        expect(messages).to.have.length(0);
+        expect(callback).to.be.a('function');
+        done();
+      };
+      inject(function(chat) {
+        chatFactory = chat;
+      });
+    });
+
+    describe('yjs events handler', function() {
+      beforeEach(function() {
+        var self = this;
+        this.yArraySynchronizerMock = function(channel, messages, callback) {
+          callback({
+            observe: function(callback) {
+              self.handler = callback;
+            }
+          });
+        };
+        inject(function(chat) {
+          chatFactory = chat;
+        });
+      });
+
+      it('should loop over events array and send rootScope events', function() {
+        var events = [
+          {
+            type: 'insert',
+            value: 'test1',
+            position: 0
+          },
+          {
+            type: 'insert',
+            value: 'test2',
+            position: 2
+          }
+        ];
+
+        var received = [];
+        $rootScope.$on('chat:message:received', function(evt, data) {
+          received.push(data);
+        });
+        this.handler(events);
+        expect(received).to.deep.equal(['test1', 'test2']);
+      });
+
+      it('should increment the unread count if the window is not opened', function() {
+        var events = [
+          {
+            type: 'insert',
+            value: 'test1',
+            position: 0
+          },
+          {
+            type: 'insert',
+            value: 'test2',
+            position: 2
+          }
+        ];
+
+        var received = [];
+        expect(chatFactory.opened).to.be.false;
+        $rootScope.$on('chat:message:received', function(evt, data) {
+          received.push(data);
+        });
+        this.handler(events);
+        expect(chatFactory.unread).to.equal(2);
+      });
+
+      it('should not increment the unread count if the window is opened', function() {
+        var events = [
+          {
+            type: 'insert',
+            value: 'test1',
+            position: 0
+          },
+          {
+            type: 'insert',
+            value: 'test2',
+            position: 2
+          }
+        ];
+
+        var received = [];
+        chatFactory.toggleWindow();
+        expect(chatFactory.opened).to.be.true;
+        $rootScope.$on('chat:message:received', function(evt, data) {
+          received.push(data);
+        });
+        this.handler(events);
+        expect(chatFactory.unread).to.equal(0);
+      });
     });
   });
 
